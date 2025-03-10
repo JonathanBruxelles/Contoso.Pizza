a7db2b89100ced28d301ea02c884dfc42cb57131
GodPay Team <148089056+JonathanBruxelles@users.noreply.github.com>
2025-03-11 00:05:18 +0100

    Add project files.

---
+++ Backend/Contoso.Pizza.AdminApi.MVC/Program.cs
@@ -0,0 +1,66 @@
+
+using Contoso.Pizza.AdminApi.Services.Extensions;
+using Contoso.Pizza.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Contoso.Pizza.AdminApi.MVC;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        //Register Contoso App Services
+        AddApplicationServices(builder);
+
+        //Register MVC Services
+        AddMvcServices(builder);
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        ConfigureMvcServices(app);
+
+
+        app.CreateDbIfNotExists();
+
+        app.Run();
+    }
+
+    private static void ConfigureMvcServices(WebApplication app)
+    {
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+    }
+
+    private static void AddMvcServices(WebApplicationBuilder builder)
+    {
+
+        //// Add DbContext with connection string
+        //builder.Services.AddDbContext<ContosoPizzaDataContext>(options =>
+        //    options.UseSqlServer(builder.Configuration.GetConnectionString("ContosoPizza")));
+
+
+        builder.Services.AddControllers();
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+
+    }
+
+    private static void AddApplicationServices(WebApplicationBuilder builder)
+    {
+        builder.Services.AddContosoPizzaServices(builder.Configuration,
+                                                 builder.Environment.IsProduction());
+    }
+}
